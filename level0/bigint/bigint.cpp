6ff7d132ac0db7296162eaa45b116a34730e2a3a
batuhankiskac <kiskacbatuhan@gmail.com>
2026-07-13 20:10:55 +0300

    Refactor bigint class for improved readability and consistency

--- level0/bigint/bigint.cpp
+++ level0/bigint/bigint.cpp
@@ -3,8 +3,8 @@
 #include <cstdlib>
 
 bigint::bigint() : str("0") {}
-bigint::bigint(unsigned int num)
-{
+
+bigint::bigint(unsigned int num) {
 	if (num == 0) {
 		str = "0";
 		return;
@@ -16,31 +16,35 @@ bigint::bigint(unsigned int num)
 	}
 	str.assign(tmp.rbegin(), tmp.rend());
 }
+
 bigint::bigint(const bigint& src) : str(src.str) {}
 
-bigint& bigint::operator=(const bigint& src)
-{
+bigint& bigint::operator=(const bigint& src) {
 	str = src.str;
 	return *this;
 }
 
-bigint bigint::operator+(const bigint& o) const
-{
+bigint bigint::operator+(const bigint& o) const {
 	std::string a = str, b = o.str;
 	std::reverse(a.begin(), a.end());
 	std::reverse(b.begin(), b.end());
-	if (a.size() < b.size()) a.append(b.size() - a.size(), '0');
-	if (b.size() < a.size()) b.append(a.size() - b.size(), '0');
+	
+	if (a.size() < b.size())
+		a.append(b.size() - a.size(), '0');
+	if (b.size() < a.size())
+		b.append(a.size() - b.size(), '0');
 
 	std::string res;
 	int carry = 0;
-	for (size_t i = 0; i < a.size(); i++)
-	{
+	for (size_t i = 0; i < a.size(); i++) {
 		int sum = (a[i] - '0') + (b[i] - '0') + carry;
 		carry = sum / 10;
 		res.push_back((sum % 10) + '0');
 	}
-	if (carry) res.push_back(carry + '0');
+
+	if (carry)
+		res.push_back(carry + '0');
+
 	std::reverse(res.begin(), res.end());
 
 	bigint t;
@@ -48,46 +52,83 @@ bigint bigint::operator+(const bigint& o) const
 	return t;
 }
 
-bigint& bigint::operator+=(const bigint& o) { return *this = *this + o; }
-bigint& bigint::operator++() { return *this = *this + bigint(1); }
+bigint& bigint::operator+=(const bigint& o) {
+	return *this = *this + o;
+}
+
+bigint& bigint::operator++() {
+	return *this = *this + bigint(1);
+}
+
 bigint bigint::operator++(int) {
 	bigint tmp = *this;
 	++(*this);
 	return tmp;
 }
 
-bigint bigint::operator<<(unsigned int n) const
-{
+bigint bigint::operator<<(unsigned int n) const {
 	bigint t = *this;
 	t.str.append(n, '0');
 	return t;
 }
 
-bigint bigint::operator>>(unsigned int n) const
-{
+bigint bigint::operator>>(unsigned int n) const {
 	bigint t = *this;
 	if (n >= t.str.size()) t.str = "0";
 	else t.str.erase(t.str.size() - n);
 	return t;
 }
 
-bigint& bigint::operator<<=(unsigned int n) { return *this = *this << n; }
-bigint& bigint::operator>>=(unsigned int n) { return *this = *this >> n; }
+bigint& bigint::operator<<=(unsigned int n) {
+	return *this = *this << n;
+}
 
-bigint bigint::operator<<(const bigint& o) const { return *this << (unsigned)atoi(o.str.c_str()); }
-bigint bigint::operator>>(const bigint& o) const { return *this >> (unsigned)atoi(o.str.c_str()); }
-bigint& bigint::operator<<=(const bigint& o) { return *this = *this << o; }
-bigint& bigint::operator>>=(const bigint& o) { return *this = *this >> o; }
+bigint& bigint::operator>>=(unsigned int n) {
+	return *this = *this >> n;
+}
 
-bool bigint::operator==(const bigint& o) const { return str == o.str; }
-bool bigint::operator<(const bigint& o) const
-{
-	if (str.size() != o.str.size()) return str.size() < o.str.size();
+bigint bigint::operator<<(const bigint& o) const {
+	return *this << (unsigned)atoi(o.str.c_str());
+}
+
+bigint bigint::operator>>(const bigint& o) const {
+	return *this >> (unsigned)atoi(o.str.c_str());
+}
+
+bigint& bigint::operator<<=(const bigint& o) {
+	return *this = *this << o;
+}
+
+bigint& bigint::operator>>=(const bigint& o) {
+	return *this = *this >> o;
+}
+
+bool bigint::operator==(const bigint& o) const {
+	return str == o.str;
+}
+
+bool bigint::operator<(const bigint& o) const {
+	if (str.size() != o.str.size())
+		return str.size() < o.str.size();
 	return str < o.str;
 }
-bool bigint::operator!=(const bigint& o) const { return !(*this == o); }
-bool bigint::operator>(const bigint& o) const { return o < *this; }
-bool bigint::operator<=(const bigint& o) const { return !(o < *this); }
-bool bigint::operator>=(const bigint& o) const { return !(*this < o); }
 
-std::ostream& operator<<(std::ostream& os, const bigint& b) { return os << b.str; }
+bool bigint::operator!=(const bigint& o) const {
+	return !(*this == o);
+}
+
+bool bigint::operator>(const bigint& o) const {
+	return o < *this;
+}
+
+bool bigint::operator<=(const bigint& o) const {
+	return !(o < *this);
+}
+
+bool bigint::operator>=(const bigint& o) const {
+	return !(*this < o);
+}
+
+std::ostream& operator<<(std::ostream& os, const bigint& b) {
+	return os << b.str;
+}
