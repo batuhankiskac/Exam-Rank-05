1c19891d281ecf7679fcd079c1033a64a3bd50f3
batuhankiskac <kiskacbatuhan@gmail.com>
2026-07-23 12:50:41 +0300

    Refactor life.c for improved readability by simplifying conditional statements and standardizing variable names

--- level1/Life/life.c
+++ level1/Life/life.c
@@ -7,8 +7,7 @@ static int count_neighbors(char *board, int row, int col, int width, int height)
 
 	for (int r = row - 1; r <= row + 1; r++)
 		for (int c = col - 1; c <= col + 1; c++)
-			if ((r != row || c != col)
-				&& r >= 0 && r < height && c >= 0 && c < width)
+			if ((r != row || c != col) && r >= 0 && r < height && c >= 0 && c < width)
 				count += board[r * width + c];
 	return count;
 }
@@ -35,17 +34,13 @@ int main(int argc, char **argv) {
 
 	while (read(STDIN_FILENO, &cmd, 1) == 1) {
 		if (cmd == 'w') {
-			if (row > 0)
-				row--;
+			if (row > 0) row--;
 		} else if (cmd == 's') {
-			if (row < height - 1)
-				row++;
+			if (row < height - 1) row++;
 		} else if (cmd == 'a') {
-			if (col > 0)
-				col--;
+			if (col > 0) col--;
 		} else if (cmd == 'd') {
-			if (col < width - 1)
-				col++;
+			if (col < width - 1) col++;
 		} else if (cmd == 'x') {
 			draw = !draw;
 		}
@@ -54,22 +49,21 @@ int main(int argc, char **argv) {
 	}
 
 	for (int i = 0; i < iter; i++) {
-		for (int y = 0; y < height; y++) {
-				for (int x = 0; x < width; x++) {
-					int neighbors = count_neighbors(board, y, x, width, height);
-					int idx = y * width + x;
-					next[idx] = neighbors == 3
-						|| (board[idx] && neighbors == 2);
-				}
+		for (int r = 0; r < height; r++) {
+			for (int c = 0; c < width; c++) {
+				int neighbors = count_neighbors(board, r, c, width, height);
+				int idx = r * width + c;
+				next[idx] = neighbors == 3 || (board[idx] && neighbors == 2);
 			}
+		}
 		char *temp = board;
 		board = next;
 		next = temp;
 	}
 
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++)
-			putchar(board[y * width + x] ? 'O' : ' ');
+	for (int r = 0; r < height; r++) {
+		for (int c = 0; c < width; c++)
+			putchar(board[r * width + c] ? 'O' : '.');
 		putchar('\n');
 	}
 	free(board);
